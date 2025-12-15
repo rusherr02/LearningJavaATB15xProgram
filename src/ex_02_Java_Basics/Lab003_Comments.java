83a00f36496bcdde355a58b23557146fc70e94f2
manav <manavmalhotra2020@gmail.com>
2025-12-15 06:55:20 +0530

    Update Lab001_HelloWorld.java

---
+++ src/ex_02_Java_Basics/Lab003_Comments.java
@@ -0,0 +1,15 @@
+package ex_02_Java_Basics;
+
+/**
+ * Author : Manav
+ * Course : ATB 15x
+ * Learning : Java
+ */
+
+
+public class Lab003_Comments {
+    static void main() {
+        // this is a comment
+        // this is a single line comment
+    }
+}
